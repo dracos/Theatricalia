bf647012265db0684fde0b63bdce454ee687adfd
Matthew Somerville <matthew@theatricalia.com>
2009-08-19 09:51:51 +0000

    Class for help text, all approx. has help text

--- places/forms.py
+++ places/forms.py
@@ -2,8 +2,14 @@ from django import forms
 from models import Place
 
 class PlaceForm(forms.ModelForm):
-    description = forms.CharField(widget=forms.Textarea(attrs={'cols': 30, 'rows':5}), required=False)
     class Meta:
         model = Place
         exclude = ('slug',)
+
+    def __init__(self, *args, **kwargs):
+        super(PlaceForm, self).__init__(*args, **kwargs)
+        self.fields['name'].widget.attrs = { 'size': 40 }
+        self.fields['description'].widget.attrs = { 'cols': 40, 'rows': 5 }
+        self.fields['opening_date'].help_text = 'The opening date may simply be month and year, or just year, if that is all that is known.'
+
 
