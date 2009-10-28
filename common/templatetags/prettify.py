354aeec27078213b50a213f5caf525e8964c76a9
(no author) <(no author)@f7e8df41-1764-4ae3-8a3e-7bc066515e65>
2009-10-28 21:52:07 +0000

    Nicer postcode

--- common/templatetags/prettify.py
+++ common/templatetags/prettify.py
@@ -62,6 +62,7 @@ def prettify(str):
     str = re.sub(r'\s0\s', ' zero ', str)
 
     str = re.sub(r'(\s[0-9.,]+) ([A-Za-z])', r'\1&nbsp;\2', str) # Hard space short numerical and maths expressions
+    str = re.sub(r' ([0-9][A-Z][A-Z])', r'&nbsp;\1', str)
 
     str = re.sub('\(press night\)', '<small>(press night)</small>', str)
 
