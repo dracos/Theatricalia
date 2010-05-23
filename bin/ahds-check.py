6ab2d87b15e2dcadd1001a21e324e8e5920dc1a7
Matthew Somerville <matthew@theatricalia.com>
2010-05-23 16:37:58 +0000

    Initial production migrations.

--- bin/ahds-check.py
+++ bin/ahds-check.py
@@ -1,19 +1,45 @@
 #!/usr/bin/python
 #
-# Lots of code copied from ahds-import.py
+# Based on ahds-import.py
 # This checks that the cast totals from AHDS match that in current/import revision in db
-# outputting the ones that don't match (all duplicates at the AHDS end)
+# outputting the ones that don't match (turns out all duplicates at the AHDS end)
+# Checks Production.Place entries too
 
 import re, os, sys
 sys.path.append('../../')
 sys.path.append('../')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'theatricalia.settings'
 
-from productions.models import Part
+from productions.models import Part, Place
 from reversion.models import Version
 
 def fix_dots(s):
     return s.title().replace('.', ' ').replace('  ', ' ').strip()
+
+# Manual list from some already existing Places
+theatre_lookup = {
+   "Comedy, London": "Comedy Theatre, London",
+   "Duchess, London": "Duchess Theatre, London",
+   "Globe, London": "Globe Theatre, London",
+   "King's, Glasgow": "King's Theatre, Glasgow",
+   "Kingsway, London": "Kingsway Theatre, London",
+   "Lyceum, Edinburgh": "Lyceum Theatre, Edinburgh",
+   "Lyric, London": "Lyric Theatre, London",
+   "Malvern": "Malvern Theatre, Worcestershire",
+   "New, London": "New Theatre, London",
+   "Palace, Manchester": "Palace Theatre, Manchester",
+   "Piccadilly, London": "Piccadilly Theatre, London",
+   "Playhouse, London": "Playhouse Theatre, London",
+   "Prince's, Manchester": "Prince's Theatre, Manchester",
+   "Queen's, London": "Queen's Theatre, London",
+   "Regent, London": "Regent Theatre, London",
+   "Royalty, London": "The Royalty Theatre, London",
+   "S. Bernhardt, Paris": "Sarah Bernhardt Theatre des Nations",
+   "S.m.t., Stratford": "Shakespeare Memorial Theatre, Stratford-upon-Avon",
+   "Vaudeville, London": "Vaudeville Theatre, London",
+   "B'ham Rep": "Birmingham Repertory Theatre",
+}
+
 
 for n in range(1, 67):
     file = open('../data/ahds/lists/%d' % n).read()
@@ -24,7 +50,16 @@ for n in range(1, 67):
         play, author, director, designer, theatre, first, last = re.findall('<td[^>]*>\s*(.*?)\s*</td>(?s)', play_row)
         if play == '<b>Play</b>':
             continue
-        data = {}
+        if theatre in theatre_lookup:
+            theatre = theatre_lookup[theatre]
+        data = {
+            'play': play,
+            'theatre': theatre,
+            'first': re.sub('^(\d\d)/(\d\d)/(\d+)$', r'19\3-\2-\1', re.sub('^(\d\d)/(\d)/(\d+)$', r'19\3-0\2-\1', re.sub('^(\d)/(\d\d)/(\d+)$', r'19\3-\2-0\1', re.sub('^(\d)/(\d)/(\d+)$', r'19\3-0\2-0\1', first)))),
+            'last':  re.sub('^(\d\d)/(\d\d)/(\d+)$', r'19\3-\2-\1', re.sub('^(\d\d)/(\d)/(\d+)$', r'19\3-0\2-\1', re.sub('^(\d)/(\d\d)/(\d+)$', r'19\3-\2-0\1', re.sub('^(\d)/(\d)/(\d+)$', r'19\3-0\2-0\1', last)))),
+        }
+        if author:
+            data['author'] = [ fix_dots(x) for x in re.split(', ', author) ]
         if director:
             data['director'] = [ fix_dots(x) for x in re.split(', ', director) ]
         if designer:
@@ -32,8 +67,8 @@ for n in range(1, 67):
             if data['designer'][0]=='Voytek':
                 data['designer'].append('')
 
-        m = re.match('<a href="birminghamrepdetails\.do\?id=(\d+)">(.*?)</a>$', play) # Always will match
-        data['id'], play = m.groups()
+        m = re.match('<a href="birminghamrepdetails\.do\?id=(\d+)">(.*?)</a>$', data['play']) # Always will match
+        data['id'], data['play'] = m.groups()
         details = open('../data/ahds/details/%s' % data['id']).read()
         m = re.search('<table width="600px" cellpadding="3" cellspacing="0" class="table01">(.*?)</table>(?s)', details)
         table = m.group(1)
@@ -44,6 +79,18 @@ for n in range(1, 67):
             value = re.sub('</?b>', '', value)
             if key == 'Cast':
                 data['cast'] = [ [ fix_dots(y) for y in re.split(', ', x)] for x in re.split('\s*<br>\s*', value) if x ]
+
+        places = Place.objects.filter(production__source__endswith='=%s">AHDS record</a>)' % data['id'])
+        if len(places)==1 and places[0].place.name == data['theatre']:
+            continue
+        #if 'theatre' in data and data['theatre']:
+            #location = add_theatre(data['theatre'])
+            #ProductionPlace.objects.get_or_create(production=production, place=location, start_date=data['first'], end_date=data['last'])
+        #else:
+            #location, created = Place.objects.get_or_create(name='Unknown')
+            #ProductionPlace.objects.get_or_create(production=production, place=location, start_date=data['first'], end_date=data['last'])
+        print places, data['theatre']
+        continue
 
         total = 0
         for c in data['cast']:
@@ -62,5 +109,5 @@ for n in range(1, 67):
         parts = parts.count()
         if orig_parts == total:
             continue
-        print data['id'], total, parts, orig_parts, play
+        print data['id'], total, parts, orig_parts, data['play']
 
