9a32b213256f9c089c331c14743f39d5e5893715
Matthew Somerville <matthew-github@dracos.co.uk>
2023-11-19 12:44:47 +0000

    Move cleanup connect to ready.

--- photos/apps.py
+++ photos/apps.py
@@ -3,12 +3,12 @@ from django_cleanup.signals import cleanup_pre_delete
 from sorl.thumbnail import delete
 
 
-class PhotoConfig(AppConfig):
-    name = 'photos'
-
-
 def sorl_delete(**kwargs):
     delete(kwargs['file'])
 
 
-cleanup_pre_delete.connect(sorl_delete)
+class PhotoConfig(AppConfig):
+    name = 'photos'
+
+    def ready(self):
+        cleanup_pre_delete.connect(sorl_delete, dispatch_uid="photo_cleanup")
